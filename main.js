523a8ddf713bad421bc771c0cb2e7e226e685fa1
edwin <edwinbanago692@gmail.com>
2023-11-27 11:42:04 +0800

    Added a script to mark panels as well as the icon of it.

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 let player1Move = true;
 let player2Move = false;
 
-let board = [false, false, false, false, false, false, false, false, false];
-console.log(board);
+let boardBool = [false, false, false, false, false, false, false, false, false];
+let boardIcon = ["", "", "", "", "", "", "", "", ""];
+
+console.log(boardBool);
+console.log(boardIcon);
 
 function changeColor(colorChoice) {
   document.getElementById("bot-name").style.color = colorChoice;
@@ -12,43 +15,86 @@ function setIconX(id, icon) {
   if (player1Move) {
     document.getElementById(id).src = "./images/x.svg";
     document.getElementById(id).style.width = "150px";
+    markBoard(id);
     player1Move = false;
     player2Move = true;
-    console.log(player1Move);
-    console.log(player2Move);
-
-    identifyPosition(id);
-
+    
   } else if (player2Move) {
     document.getElementById(id).src = "./images/o.svg";
     document.getElementById(id).style.width = "125px";
+    markBoard(id);
+    console
     player1Move = true;
     player2Move = false;
-
-    identifyPosition(id);
   }
 }
 
-function identifyPosition(id) {
-    if (id == "p0-icon") {
-      board[0] = true;
-    } else if (id == "p1-icon") {
-      board[1] = true;
-    } else if (id == "p2-icon") {
-      board[2] = true;
-    } else if (id == "p3-icon") {
-      board[3] = true;
-    } else if (id == "p4-icon") {
-      board[4] = true;
-    } else if (id == "p5-icon") {
-      board[5] = true;
-    } else if (id == "p6-icon") {
-      board[6] = true;
-    } else if (id == "p7-icon") {
-      board[7] = true;
-    } else if (id == "p8-icon") {
-      board[8] = true;
+function markBoard(id) {
+  if (id == "p0-icon") {
+    boardBool[0] = true;
+    if (player1Move) {
+      boardIcon[0] = "x";
+    } else if (player2Move) {
+      boardIcon[0] = "o";
     }
+  } else if (id == "p1-icon") {
+    boardBool[1] = true;
+    if (player1Move) {
+      boardIcon[1] = "x";
+    } else if (player2Move) {
+      boardIcon[1] = "o";
+    }
+  } else if (id == "p2-icon") {
+    boardBool[2] = true;
+    if (player1Move) {
+      boardIcon[2] = "x";
+    } else if (player2Move) {
+      boardIcon[2] = "o";
+    }
+  } else if (id == "p3-icon") {
+    boardBool[3] = true;
+    if (player1Move) {
+      boardIcon[3] = "x";
+    } else if (player2Move) {
+      boardIcon[3] = "o";
+    }
+  } else if (id == "p4-icon") {
+    boardBool[4] = true;
+    if (player1Move) {
+      boardIcon[4] = "x";
+    } else if (player2Move) {
+      boardIcon[4] = "o";
+    }
+  } else if (id == "p5-icon") {
+    boardBool[5] = true;
+    if (player1Move) {
+      boardIcon[5] = "x";
+    } else if (player2Move) {
+      boardIcon[5] = "o";
+    }
+  } else if (id == "p6-icon") {
+    boardBool[6] = true;
+    if (player1Move) {
+      boardIcon[6] = "x";
+    } else if (player2Move) {
+      boardIcon[6] = "o";
+    }
+  } else if (id == "p7-icon") {
+    boardBool[7] = true;
+    if (player1Move) {
+      boardIcon[7] = "x";
+    } else if (player2Move) {
+      boardIcon[7] = "o";
+    }
+  } else if (id == "p8-icon") {
+    boardBool[8] = true;
+    if (player1Move) {
+      boardIcon[8] = "x";
+    } else if (player2Move) {
+      boardIcon[8] = "o";
+    }
+  }
 
-    console.log(board);
+  console.log(boardBool);
+  console.log(boardIcon)
 }
